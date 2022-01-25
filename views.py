3fcc7fea734029cd58996efa07fca4c4eaefe3a6
unaibp96 <83061201+unaibp96@users.noreply.github.com>
2022-01-25 10:38:36 +0100

    modificando el views.py galeria

---
+++ views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+from django.http import HttpResponse
+
+
+def index(request):
+    return HttpResponse("<img src="'https://phantom-marca.unidadeditorial.es/1dd931abcac95b6b9e302f205e0552fb/crop/29x58/773x477/resize/1320/f/jpg/assets/multimedia/imagenes/2021/12/22/16401669242106.jpg'"><p>Calladito miserable</p> ")
