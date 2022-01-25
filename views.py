8e726d9bd1cba53ad4b65724833d1c49d12f7d3b
unaibp96 <83061201+unaibp96@users.noreply.github.com>
2022-01-25 10:40:35 +0100

    añadiendo un miserable

--- views.py
+++ views.py
@@ -6,4 +6,4 @@ from django.http import HttpResponse
 
 
 def index(request):
-    return HttpResponse("<img src="'https://phantom-marca.unidadeditorial.es/1dd931abcac95b6b9e302f205e0552fb/crop/29x58/773x477/resize/1320/f/jpg/assets/multimedia/imagenes/2021/12/22/16401669242106.jpg'"><p>Calladito miserable</p> ")
+    return HttpResponse("<img src="'https://phantom-marca.unidadeditorial.es/1dd931abcac95b6b9e302f205e0552fb/crop/29x58/773x477/resize/1320/f/jpg/assets/multimedia/imagenes/2021/12/22/16401669242106.jpg'"><p>Calladito miserable miserableeeeeeee</p> ")
